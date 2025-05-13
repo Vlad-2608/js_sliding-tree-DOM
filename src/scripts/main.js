7e0a558676d401fa6927e39c068d0c8e1997e845
unknown <vladhafarov@gmail.com>
2025-05-13 21:57:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  function toggleSublist(evt) {
+    const target = evt.target;
+
+    if (target.tagName === 'SPAN') {
+      const parenLi = target.parentNode;
+      const sublist = parenLi.querySelector('ul');
+
+      if (sublist) {
+        sublist.style.display = sublist.style.display === 'none' ? '' : 'none';
+      }
+    }
+  }
+
+  function wrapTextNodes(node) {
+    node.childNodes.forEach((child) => {
+      if (
+        child.nodeType === Node.TEXT_NODE &&
+        child.textContent.trim() !== ''
+      ) {
+        const span = document.createElement('span');
+
+        span.textContent = child.textContent;
+        child.replaceWith(span);
+      } else if (child.nodeType === Node.ELEMENT_NODE) {
+        wrapTextNodes(child);
+      }
+    });
+  }
+
+  wrapTextNodes(tree);
+  tree.addEventListener('click', toggleSublist);
+});
